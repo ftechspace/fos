2848601cbdfd96e774180306acafef38af7d562d
fortunetede <fortunetedegh@gmail.com>
2022-12-14 13:06:42 +0000

    Minimal Rust Kernel: print Hello world using gemu

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,26 @@
 #![no_main] //telling rust compiler  we don't want to use the normal entry point chain. 
             //we will rewrite the crtO entry point directly
 
-/// This function is called on panic.
 use core::panic::PanicInfo;
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-// end of function
+
+static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}  
+// end of function
+
